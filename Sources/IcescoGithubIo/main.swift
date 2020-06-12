e0b23d1dafce46a40d9063d497797ccaec9b72e0
Francesco Bianco <f.bianco@aliaslab.net>
2020-06-12 19:09:33 +0200

    Better title, empty site

--- Sources/IcescoGithubIo/main.swift
+++ Sources/IcescoGithubIo/main.swift
@@ -21,5 +21,89 @@ struct IcescoGithubIo: Website {
     var imagePath: Path? { nil }
 }
 
+private extension Node where Context == HTML.BodyContext {
+    
+    static func wrapper(_ nodes: Node...) -> Node {
+        .div(.class("wrapper"), .group(nodes))
+    }
+    
+    static func itemList<T: Website>(for items: [Item<T>], on site: T) -> Node {
+        return .ul(
+            .class("item-list"),
+            .forEach(items) { item in
+                .li(
+                    .article(
+                        .h1(.a(
+                            .href(item.path),
+                            .text(item.title)
+                            )),
+                        .p(.text(item.description))
+                    )
+                )
+            }
+        )
+    }
+}
+
+struct MyHTMLFactory<Site: Website>: HTMLFactory {
+    
+    typealias Site = IcescoGithubIo
+    
+    
+    func makeIndexHTML(for index: Index, context: PublishingContext<IcescoGithubIo>) throws -> HTML {
+        HTML(
+            .head(for: index, on: context.site),
+            .body(
+                .header(
+                    .wrapper(
+                        .nav(
+                            .class("site-name"),
+                            .text(context.site.name)
+                        )
+                    )
+                ),// HEADER
+                
+                .wrapper(
+                    
+                )
+            )
+        )
+    }
+    
+    func makeSectionHTML(for section: Section<IcescoGithubIo>, context: PublishingContext<IcescoGithubIo>) throws -> HTML {
+        HTML(
+            .head(for: section, on: context.site)
+        )
+    }
+    
+    func makeItemHTML(for item: Item<IcescoGithubIo>, context: PublishingContext<IcescoGithubIo>) throws -> HTML {
+        HTML(
+            .head(for: item, on: context.site)
+        )
+    }
+    
+    func makePageHTML(for page: Page, context: PublishingContext<IcescoGithubIo>) throws -> HTML {
+        try makeIndexHTML(for: context.index, context: context)
+    }
+    
+    func makeTagListHTML(for page: TagListPage, context: PublishingContext<IcescoGithubIo>) throws -> HTML? {
+        nil
+    }
+    
+    func makeTagDetailsHTML(for page: TagDetailsPage, context: PublishingContext<IcescoGithubIo>) throws -> HTML? {
+        nil
+    }
+}
+
+extension Theme where Site == IcescoGithubIo {
+    
+    static var myCustomTheme: Theme {
+      
+        Theme(htmlFactory: MyHTMLFactory<IcescoGithubIo>(),
+        resourcePaths: Set<Path>())
+    
+    }
+}
+
 // This will generate your website using the built-in Foundation theme:
-try IcescoGithubIo().publish(withTheme: .foundation)
+try IcescoGithubIo().publish(withTheme: .myCustomTheme)
